bcdd38131202b6bf69b32c6d2b70476b00ae87d5
judsonpaiva <judson.paiva@hotmail.com>
2022-03-28 18:42:54 +0100

    enviando e recebendo valores

--- app/src/main/java/com/example/activitycomresult/MainActivity.java
+++ app/src/main/java/com/example/activitycomresult/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.activitycomresult;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txt_utilizador;
+    Button btn_enviar;
+    String nome = "Utilizador";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txt_utilizador = findViewById(R.id.txt_utilizador);
+        btn_enviar = findViewById(R.id.btn_enviar);
+
+        txt_utilizador.setText("Olá "+nome);
+
+        btn_enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TelaDoisActivity.class);
+                intent.putExtra("keyNome", nome);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/activitycomresult/TelaDoisActivity.java
+++ app/src/main/java/com/example/activitycomresult/TelaDoisActivity.java
@@ -2,13 +2,35 @@ package com.example.activitycomresult;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class TelaDoisActivity extends AppCompatActivity {
+    EditText edt_nome;
+    Button btn_voltar;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_dois);
+
+        edt_nome = findViewById(R.id.edt_nome);
+        btn_voltar = findViewById(R.id.btn_voltar);
+
+        intent = getIntent();
+        System.out.println(intent.getExtras().getString("keyNome"));
+
+        btn_voltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                finish();
+            }
+        });
+
     }
 }
